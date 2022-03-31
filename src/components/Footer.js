904ae2d50fe4a4a03faab07cf7ecb4e5ed4d2087
Devanshu Pandey <devanshusenp@gmail.com>
2022-03-31 01:00:58 -0500

    navbar & footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Logo from "../assets/Logo.png";
+import { BsInstagram, BsTwitter, BsFacebook } from "react-icons/bs";
 
 function Footer() {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <img className="logo" src={Logo} />
+      <div className="mail">
+        Contact Us:{" "}
+        <a href="mailto: info@improvethedream.org">info@ImproveTheDream.org</a>
+      </div>
+      <div className="media">
+        <a
+          href="https://www.instagram.com/improvethedream/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+        <a
+          href="https://twitter.com/improvethedream"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsTwitter />
+        </a>
+        <a
+          href="https://www.facebook.com/ImproveTheDreamOrg/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsFacebook />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Logo from "../assets/Logo.png";
 
 function Navbar() {
   return (
     <div className="Navbar">
-      <div className="content">
-        <button>home</button>
-        <button>about</button>
-        <button>stories</button>
-        <button>take action</button>
+      <div className="content open-sans">
+        <img className="logo" src={Logo} />
+        <div className="links">
+          <a>home</a>
+          <a>about</a>
+          <a>stories</a>
+          <a>take action</a>
+        </div>
       </div>
     </div>
   );
