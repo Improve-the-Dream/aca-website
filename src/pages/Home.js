3f3281eadf6e907632686da364f1e4c8ab893371
Devanshu Pandey <devanshusenp@gmail.com>
2023-05-21 12:04:27 -0500

    updated bill links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,22 +17,22 @@ function Home(props) {
         <button
           onClick={() =>
             window.open(
-              "https://www.congress.gov/bill/117th-congress/house-bill/4331/text",
+              "https://www.congress.gov/bill/118th-congress/house-bill/3442?q=%7B%22search%22%3A%5B%22Deborah+ross%22%5D%7D&s=5&r=2",
               "_blank"
             )
           }
         >
-          <CgFileDocument /> H.R. 4331
+          <CgFileDocument /> H.R. 3442
         </button>
         <button
           onClick={() =>
             window.open(
-              "https://www.congress.gov/bill/117th-congress/senate-bill/2753/text?q=%7B%22search%22%3A%5B%22chamberActionDateCode%3A%5C%222021-09-15%7C117%7C10000%5C%22+AND+billIsReserved%3A%5C%22N%5C%22%22%5D%7D&r=5&s=1",
+              "https://www.congress.gov/bill/118th-congress/senate-bill/1667?q=%7B%22search%22%3A%5B%22Padilla%22%5D%7D&s=8&r=1",
               "_blank"
             )
           }
         >
-          <CgFileDocument /> S. 2753
+          <CgFileDocument /> S. 1667
         </button>
       </div>
     </div>
